7ddd3197da6990e0300c3bc666e5d0eaf962401b
よしだ <56685063+yoshida-prog@users.noreply.github.com>
2021-02-14 01:44:33 +0900

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,11 +1,19 @@
+let startBtn = document.getElementById('fbBtn');
+
+startBtn.addEventListener('click', function(){
+  fizzbuzz();
+}, false);
+
 function addElement(fb, forb){
-  const newP = document.createElement("p");
-  newP.innerHTML = fb + "," + forb;
-  document.getElementById("fizzbuzzBox").appendChild(newP);
+  let fizzbuzzBox = document.getElementById('fizzbuzzBox');
+  let content = fb + ',' + forb;
+  const newP = document.createElement('p');
+  const newContent = document.createTextNode(content);
+  fizzbuzzBox.appendChild(newP).appendChild(newContent);
 }
 
 function documentReset(){
-  const element = document.getElementById("fizzbuzzBox");
+  const element = document.getElementById('fizzbuzzBox');
   while (element.firstChild){
     element.removeChild(element.firstChild);
   }
@@ -18,31 +26,19 @@ function fizzbuzz(){
   if(fizz.match(/[0-9]{1,2}/g) == fizz && buzz.match(/[0-9]{1,2}/g) == buzz){
     fizz = Number(fizz);
     buzz = Number(buzz);
-    let flug = 0;
-    initialFizz = fizz;
-    initialBuzz = buzz;
-    function fizz_plus(){
-      fizz = fizz + initialFizz;
-    }
-    function buzz_plus(){
-      buzz = buzz + initialBuzz;
-    }
-    for(i=1; i<100; i++){
-      if(i%fizz == 0 && i%buzz == 0){
-        addElement(fizz, "fizzbuzz");
-        fizz_plus();
-        buzz_plus();
-      }else if(i%fizz == 0){
-        addElement(fizz, "fizz");
-        fizz_plus();
-      }else if(i%buzz == 0){
-        addElement(buzz, "buzz");
-        buzz_plus();
+    for(let i=1; i<100; i++){
+      if(i%fizz === 0 && i%buzz === 0){
+        addElement(i, 'fizzbuzz');
+      }else if(i%fizz === 0){
+        addElement(i, 'fizz');
+      }else if(i%buzz === 0){
+        addElement(i, 'buzz');
       }
     }
   }else{
-    alert("2桁以下の自然数で入力してください");
-    form.fizz.value = "";
-    form.buzz.value = "";
+    alert('2桁以下の自然数で入力してください');
+    form.fizz.value = '';
+    form.buzz.value = '';
   }
 }
+
